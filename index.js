709c21172b45f2bb935fedcecff939f9fb7e65a5
shanky199912 <shash.tew@gmail.com>
2020-05-29 22:35:40 +0530

    update

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hi');
+const express = require('express') //import express from 'express'
+const app = express()
+
+const PORT = 4000;
+// respond with "hello world" when a GET request is made to the homepage
+
+//now we will handle the listening 
+function handlelistening(){
+  console.log(`Listening on http://localhost:${PORT}`); //template literals.
+}
+
+app.listen(PORT, handlelistening); //sever starts listening when we run it.
