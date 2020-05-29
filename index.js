bd00108f087dc10c952f3d22c83a5da26bd13882
shanky199912 <shash.tew@gmail.com>
2020-05-30 00:42:30 +0530

    update

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
 import express from "express";
-
 // const express = require('express') //import express from 'express'
 const app = express()
 const PORT = 4000;
 // respond with "hello world" when a GET request is made to the homepage
 
 //now we will handle the listening 
-function handlelistening(){
+const handlelistening = () =>
   console.log(`Listening on http://localhost:${PORT}`); //template literals.
-}
 
-function handleHome(req, res){
-  // console.log(req);
-  res.send("HELLO WORLD");
-}
+const handleHome = (req, res) => res.send("HELLO WORLD");
 
-function handleProfile(req, res){
-  // console.log("Reqeust result :" + res);
-  res.send("Hello Shashank Tewari");
-}
+// console.log("Reqeust result :" + res);
+const handleProfile = (req, res) => res.send("Hello Shashank Tewari");
 
 app.get("/profile", handleProfile); //listening on profile page
 
